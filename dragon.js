76171ac77a1a46f28fe9d5757c3f15b1dda33027
ish-kafel <1136461977@qq.com>
2018-07-30 18:55:02 +0800

    dragon

--- dragon.js
+++ dragon.js
@@ -1,9 +1,4 @@
-﻿function oc(x){
- if(x.value==x.id){x.value=""}
-}
-function bl(x){
- if(x.value==""){x.value=x.id}
-}
+﻿
 function dIteration(direct,depth){
   var arr=[]
   arr.push(direct)
@@ -17,39 +12,47 @@ function dIteration(direct,depth){
   return arr
 }
 
+function dDraw(canvas,arr,startX,startY,leng){
+  var x=[],y=[]
+  var Color = net.brehaut.Color;
+
+  var cx=canvas.getContext("2d")
+  cx.clearRect(0,0,canvas.width,canvas.height);  
+  cx.lineWidth=2
+
+  for(var ll=0;ll<arr.length;ll++){
+    x[ll]=arr[ll]=="r"?leng:(arr[ll]=="l"?-leng:0)
+    y[ll]=arr[ll]=="d"?leng:(arr[ll]=="u"?-leng:0)
+  }
+
+  for(var k=0;k<arr.length;k++){
+    cx.beginPath();
+    cx.moveTo(startX,startY)
+    cx.strokeStyle=Color("hsl("+360*k/arr.length+", 100%, 50%)")
+    startX+=x[k]
+    startY+=y[k]
+    cx.lineTo(startX,startY)
+    cx.stroke()
+  }
+}
+
 function draw(){
   var depth=$("#depth").value
   var len=+$("#len").value
   var direct=$("#direct").value
   var m=+$("#dx").value
   var n=+$("#dy").value
+
   var name = ["depth","len","direct","dx","dy"]
   for(var i=0;i<name.length;i++){
-     if($("#"+name[i]).value==name[i]){alert("请输入"+name[i])}
+     if($("#"+name[i]).value==name[i]){
+       alert("请输入"+name[i])
+       break;
+     }
   }
-  if(direct.match(/[^dlru]/)){alert("初始方向请输入u,d,l或r")}
-  else{
-    var a=[],x=[],y=[]
-
-    var Color = net.brehaut.Color;
+  if(direct.match(/[^dlru]/)&&direct!="direct"){alert("初始方向请输入u,d,l或r")}
+  else if(direct!="direct"){
     a=dIteration(direct,depth)
-    var cx=ca.getContext("2d")
-    cx.clearRect(0,0,ca.width,ca.height);  
-    cx.lineWidth=2
-
-    for(var ll=0;ll<a.length;ll++){
-      x[ll]=a[ll]=="r"?len:(a[ll]=="l"?-len:0)
-      y[ll]=a[ll]=="d"?len:(a[ll]=="u"?-len:0)
-    }
-
-    for(var k=0;k<a.length;k++){
-      cx.beginPath();
-      cx.moveTo(m,n)
-      cx.strokeStyle=Color("hsl("+360*k/a.length+", 100%, 50%)")
-      m+=x[k]
-      n+=y[k]
-      cx.lineTo(m,n)
-      cx.stroke()
-    }
+    dDraw(ca,a,m,n,len)
   }
 }
