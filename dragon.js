6d844931f78b5ddf13edf65e4755bfebf431c789
ish-kafel <1136461977@qq.com>
2018-08-06 13:18:57 +0800

    dragon

--- dragon.js
+++ dragon.js
@@ -1,58 +1,67 @@
 ﻿
-function dIteration(direct,depth){
-  var arr=[]
+function dIteration(direct, depth) {
+  var arr = []
   arr.push(direct)
-  for(var j=0;j<depth;j++){
-    var b=arr.slice().reverse()
-    b.forEach(function(e,i,ar){
-      ar[i]=e=="r"?"u":(e=="u"?"l":(e=="l"?"d":"r"))
+  for (var j = 0; j < depth; j++) {
+    var b = arr.slice().reverse()
+    b.forEach(function (e, i, ar) {
+      ar[i] = e == "r" ? "u" : (e == "u" ? "l" : (e == "l" ? "d" : "r"))
     })
-    arr=arr.concat(b)
+    arr = arr.concat(b)
   }
   return arr
 }
 
-function dDraw(canvas,arr,startX,startY,leng){
-  var x=[],y=[]
-  var Color = net.brehaut.Color
-
-  var cx=canvas.getContext("2d")
-  cx.clearRect(0,0,canvas.width,canvas.height) 
-  cx.lineWidth=2
-
-  for(var ll=0;ll<arr.length;ll++){
-    x[ll]=arr[ll]=="r"?leng:(arr[ll]=="l"?-leng:0)
-    y[ll]=arr[ll]=="d"?leng:(arr[ll]=="u"?-leng:0)
-  }
-  
-  for(var k=0;k<arr.length;k++){
-    cx.beginPath();
-    cx.moveTo(startX,startY)
-    cx.strokeStyle=Color("hsl("+360*k/arr.length+", 100%, 50%)")
-    startX+=x[k]
-    startY+=y[k]
-    cx.lineTo(startX,startY)
-    cx.stroke()
-  }
-}
-
-function draw(){
-  var depth=$("#depth").value
-  var len=+$("#len").value
-  var direct=$("#direct").value
-  var m=+$("#dx").value
-  var n=+$("#dy").value
-
-  var name = ["depth","len","direct","dx","dy"]
-  for(var i=0;i<name.length;i++){
-    if($("#"+name[i]).value==name[i]){
-      alert("请输入"+name[i])
+function draw() {
+  depth = $("#depth").value
+  len = +$("#len").value
+  direct = $("#direct").value
+  m = +$("#dx").value
+  n = +$("#dy").value
+  var name = ["depth", "len", "direct", "dx", "dy"]
+  for (var i = 0; i < name.length; i++) {
+    if ($("#" + name[i]).value == name[i]) {
+      alert("请输入" + name[i])
       break
     }
   }
-  if(direct.match(/[^dlru]/)&&direct!="direct"){alert("初始方向请输入u,d,l或r")}
-  else if(direct!="direct"){
-    a=dIteration(direct,depth)
-    dDraw(ca,a,m,n,len)
+  if (direct.match(/[^dlru]/) && direct != "direct") { alert("初始方向请输入u,d,l或r") }
+  else if (direct != "direct") {
+
+    drawOn = true
+    start.style.display = 'none'
+    stop.style.display = 'block'
+    pause.style.display = 'block'
+    a = dIteration(direct, depth)
+    cx.clearRect(0, 0, ca.width, ca.height)
+    cx.lineWidth = 2
+    for (var ll = 0; ll < a.length; ll++) {
+      x[ll] = a[ll] == "r" ? len : (a[ll] == "l" ? -len : 0)
+      y[ll] = a[ll] == "d" ? len : (a[ll] == "u" ? -len : 0)
+    }
+    k = 0
+    dDraw()
+  }
+}
+
+function dDraw() {
+  cx.beginPath()
+  cx.moveTo(m, n)
+  cx.strokeStyle = Color("hsl(" + 360 * k / a.length + ", 100%, 50%)")
+  m += x[k]
+  n += y[k]
+  cx.lineTo(m, n)
+  cx.stroke()
+  if (k != a.length - 1) {
+    if (drawOn == true) {
+      setTimeout(dDraw, 30)
+      k++
+    }
+  } else {
+    drawOn = false
+    start.style.display = 'block'
+    pause.style.display = 'none'
+    stop.style.display = 'none'
+    pause.style.display = 'none'
   }
 }
